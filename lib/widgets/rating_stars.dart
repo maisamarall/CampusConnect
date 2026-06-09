398ec02697f62b943d95b6956a5f9a765640868c
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2026-06-09 13:59:16 -0300

    feat: add RatingStars

---
+++ lib/widgets/rating_stars.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class RatingStars extends StatelessWidget {
+  final int value;
+  final ValueChanged<int> onChanged;
+
+  const RatingStars({
+    super.key,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        5,
+        (index) => IconButton(
+          iconSize: 42,
+          onPressed: () => onChanged(index + 1),
+          icon: Icon(
+            index < value ? Icons.star : Icons.star_border,
+            color: Colors.amber,
+          ),
+        ),
+      ),
+    );
+  }
+}
